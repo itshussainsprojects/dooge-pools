8deeb241d41e8a070b925f3eb7e0d7acd0632bef
Hassan ali <officialhassan005@gmail.com>
2025-11-22 23:37:33 +0500

    fixed scroll changes

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__61b839b0._.css",
-  "static/chunks/_023cd6d8._.js"
-],
-    source: "dynamic"
-});
